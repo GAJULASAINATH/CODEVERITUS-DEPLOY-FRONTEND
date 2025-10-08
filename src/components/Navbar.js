a36d72d1af93c1ec8fc9f5f70ca21d4e9c1fa2e9
Gajula Sainath <gajulasainath25@gmail.com>
2025-10-08 18:34:09 +0530

    codeveritus is live

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="container">
         <Link className="navbar-brand d-flex align-items-center" to="/">
           <img 
-            src="logot.png" 
+            src="logo.png" 
             alt="CodeVeritus Logo" 
             style={{ width: "40px", height: "40px", marginRight: "10px" }} 
           />
